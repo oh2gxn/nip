8db7031cb3c7a60e5db5f28be3b2284d4b177b2a
arasinen <arasinen>
2004-01-21 12:56:44 +0000

    Lisaa refaktorointia

--- Graph.c
+++ Graph.c
@@ -58,32 +58,36 @@ Variable* get_variables(Graph* G)
     return G->variables;
 }
 
-void make_undirected(Graph* Gu, Graph* G)
+Graph* make_undirected(Graph* G)
 {
+    Graph* Gu;   
     int i,j,n;
-    int* Gam = G->adj_matrix; /* Gam ie. G adjacency matrix */
+    int* Gam; /* Gam ie. G adjacency matrix */
+    
+    if (G == NULL || G->variables == NULL)
+        return NULL;
+
+    n = G->size; Gam = G->adj_matrix
+    Gu = new_graph(n);
+    add_all_variables(Gu, G->variables); /* XX Ongelmia tuhottaessa? */
     
     /* Create the undirected graph */
-    n = G->size;
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
             Gu->adj_matrix[i][j] = Gam[i][j] || Gam[j][i];
+
+    return Gu;
 }
 
-Graph* moralise(Graph* G)
+void moralise(Graph* Gm, Graph G)
 {
-    Graph* Gm;
     int i,j,n,v;
         
     if (G == NULL || G->variables == NULL)
         return NULL;
     
     n = G->size;
-    
-    Gm = new_graph(n);
-    add_all_variables(Gm, G->variables); /* XX Ongelmia tuhottaessa? */
-    make_undirected(Gm, G);
-    
+        
     /* Moralisation */
     for (v = 0; v < n; v++)       /* Iterate variables */
         for (i = 0; i < n; i++) 
@@ -93,8 +97,6 @@ Graph* moralise(Graph* G)
                     Gm->adj_matrix[i][j] |= G->adj_matrix[j][v];
                     Gm->adj_matrix[j][i] |= G->adj_matrix[j][v];                    
                 }
-
-    return Gm;
 }
 
 /* Not specified Graph.h -- internal helper function */
