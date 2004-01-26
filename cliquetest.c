fb784d5e97253033da09bc95a26ab9d28d6d2426
jatoivol <jatoivol>
2004-01-26 13:49:10 +0000

    Kirjoottelin testiohjelmaa ja yritin keksiä keinoa toteuttaa initialisoinnin.

--- cliquetest.c
+++ cliquetest.c
@@ -1,6 +1,12 @@
 #include "Clique.h"
 
+#define YKS_KARDINALITEETTI_VAAN 2
+
 int main(){
+  int i;
+  double c[YKS_KARDINALITEETTI_VAAN];
+  double sum = 0;
+
   /* create the variables */
   Variable variables[5];
   char name1[] = "einari";
@@ -10,9 +16,10 @@ int main(){
   char name5[] = "raimo";
   variables[0] = new_variable(name1, 3);
   variables[1] = new_variable(name2, 4);
-  variables[2] = new_variable(name3, 2);
+  variables[2] = new_variable(name3, YKS_KARDINALITEETTI_VAAN);
   variables[3] = new_variable(name4, 3);
   variables[4] = new_variable(name5, 2);
+
   /* create some cliques and sepsets (with too many shortcuts...) */
   Clique clique_pile[3];
   Sepset sepset_pile[2];
@@ -26,6 +33,27 @@ int main(){
   add_Sepset(clique_pile[1], sepset_pile[1]);
   add_Sepset(clique_pile[2], sepset_pile[1]);
 
+  /* initialization */
+
+  /* observation entry */
+  
+
+  /* propagation */
+  for(i = 0; i < 3; i++)
+    unmark_Clique(clique_pile[i]);
+  collect_evidence(NULL, NULL, clique_pile[1]);
+  distribute_evidence(clique_pile[1]);
+
+  /* marginalization */
+  marginalise(clique_pile[1], variables[2], c);
+
+  /* normalization */
+  for(int i = 0; i < YKS_KARDINALITEETTI_VAAN; i++)
+    sum += c[i];
+  if(sum != 0)
+    for(int i = 0; i < YKS_KARDINALITEETTI_VAAN; i++)
+      c[i] /= sum;
+  
   /* To be continued... */
 
   return 0;
