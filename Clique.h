e67fbd2860811d192f76c14255fcb0b5d6bf0518
mvkorpel <mvkorpel>
2004-02-09 14:51:57 +0000

    Pienen pieni muutos.

--- Clique.h
+++ Clique.h
@@ -64,6 +64,10 @@ int collect_evidence(Clique c1, Sepset s12, Clique c2);
 /* Method for passing messages between cliques. Returns an error code. */
 int message_pass(Clique c1, Sepset s, Clique c2);
 
+/* Make up a better name for this */
+/* !!! p->num_of_vars equals "length of parents + 1" !!! */
+int initialise(Clique c, Variable v, Variable parents[], potential p);
+
 /* This one calculates the probability distribution for a variable v
    according to the clique c. To make sense, the join tree should be 
    made consistent before this. 
