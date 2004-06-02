f58466a2c01f880749549a50a71588057fc92e50
arasinen <arasinen>
2004-06-02 12:33:23 +0000

    Release candidate 1

--- Graph.h
+++ Graph.h
@@ -4,42 +4,68 @@
 #include "Variable.h"
 #include "Clique.h"
 
-#define ADJM(G, i, j) (G->adj_matrix[i*G->size + j])
+#define ADJM(G, i, j) ((G)->adj_matrix[(i)*(G)->size + (j)])
 
 typedef struct {
-    int* adj_matrix;
+    int* adj_matrix; /* Two dimensional */
+    unsigned long* var_ind;
+    unsigned long min_id;
+    unsigned long max_id;
     Variable* variables;
     unsigned size;
     int top;
 } Graph; /* Esimerkki. Toteutus voi vaihtua */
-
-struct clustlist {
-    struct clustlist* next;
-    int* variable_set;
-};
-
-typedef struct clustlist Cluster_list;
 
 Graph* new_graph(unsigned n);
 /* Creates new graph.
  * Parameter n: number of variables in a graph.
  */
 
+Graph* copy_graph(Graph* G);
+/* Copies a graph. Does not create copies of variables,
+ * but does copy the array of variables.
+ * Parameter G: The graph to be copied
+ */
+
 void free_graph(Graph* G);
 /* Frees the memory used by the Graph G.
+ */
+
+int get_size(Graph* G);
+/* Returns the number of variables in the graph G.
+ * Parameter G: the graph
+ */
+
+Variable* get_variables(Graph* G);
+/* Returns the variables used in Graph G;
+ * Parameter G: the graph
+ */
+
+int get_graph_index(Graph* G, Variable v);
+/* Returns the index of v in the variable-array 
+ * Parameter G: the graph
+ * Parameter v: the variable
+ */
+
+int get_neighbours(Graph* G, Variable** neighbours, Variable v);
+/* Returns the number of neighbours of v.
+ * Parameter G: the graph
+ * Parameter neighbours: a pointer to a variable array
+                         will contain the neighbouring variables after call
+ * Parameter v: the variable 
+ */
+
+int is_child(Graph* G, Variable parent, Variable child);
+/* Returns true, if there is a link from parent to child
+ * Parameter G: the graph
+ * Parameter parent: the suspected parent
+ * Parameter child: the suspected child
  */
 
 int add_variable(Graph* G, Variable v);
 /* Adds a new variable (ie. a node) to the graph. 
  * Parameter G: the graph
  * Parameter v: the variable to be added
- */
-
-int add_all_variables(Graph* G, Variable vars[]);
-/* Adds all variables to the graph G.
- * Parameter vars: a Variable-array, size n
- * Returns true, if success. False otherwise.
- * Bugger ye all, if ye use an array other than size n.
  */
 
 int add_child(Graph* G, Variable parent, Variable child);
@@ -49,32 +75,14 @@ int add_child(Graph* G, Variable parent, Variable child);
  * Parameter child: the child-variable
  */
 
-int get_size(Graph* G);
-/* Returns the number of variables in the graph G.
- * Parameter G: the graph
- */
-
-/* MVK: Mitä tämä tekee? Onko prototyyppi puutteellinen? */
-int get_neighbours(Graph* G, Variable** neighbours);
-
-Variable* get_variables(Graph* G);
-/* Returns the variables used in Graph G;
- * Parameter G: the graph
- */
-
-int is_child(Graph* G, Variable parent, Variable child);
-/* Returns 1 if "child" is a child of "parent" in Graph "G",
- * 0 if not.
- */
-
-void moralise(Graph* Gm, Graph* G);
+Graph* moralise(Graph* G);
 /* Moralises a DAG. (Brit. spelling)
  * Parameter G: An unmoral graph.
- * Parameter Gm becomes the moralised Graph.
+ * Returns moralised Graph.
  * Does not modify G.
  */
 
-int find_cliques(Graph* Gm, Clique* cliques_p);
+int find_cliques(Graph* Gm, Clique** cliques_p);
 /* Triangulates G and finds the cliques.
  * Parameter Gm: moralised graph
  * Parameter cliques_p: pointer to a clique array
@@ -83,5 +91,7 @@ int find_cliques(Graph* Gm, Clique* cliques_p);
  * Modifies Gm.
  */
 
+void sort_variables(Graph* G); 
+/* Internal helper */
 
 #endif /* __GRAPH_H__ */
--- src/Graph.c
+++ src/Graph.c
@@ -3,7 +3,8 @@
 #include "Graph.h"
 #include "Variable.h"
 #include "Clique.h"
-#include "Heap.h"
+#include "grphmnp/Heap.h"
+#include "grphmnp/cls2clq.h"
 
 /* Tämä ja Heap.c pitänee järjestää hieman järkevämmin ja ehkä luoda kullekin
    oma kotihakemisto. Nykyinen on epätyydyttävä ja jotenkin häiritsee.
@@ -12,89 +13,88 @@
   */
 
 
-/* XXXX SEURAAVAN REFAKTOROINTIPUUSKAN ISKIESSÄ:
-   XXXX Muunnosta Variable -> adj_matrix-indeksi tarvitaan usein
-   XXXX Closed taulukko -hash tätä varten.
-*/
-
-/* DUMMY-funktio, ehkä nimi pitäisi vaihtaa */
-int get_graph_index(Graph* G, Variable v){
-  return 0;
-}
+/*** GRAPH MANAGEMENT ***/
 
 Graph* new_graph(unsigned n)
 {
-    int i;
     Graph* newgraph = (Graph*) malloc(sizeof(Graph));
     newgraph->size = n; newgraph->top = 0;
     newgraph->adj_matrix = (int*) calloc(n*n, sizeof(int));
     memset(newgraph->adj_matrix, 0, n*n*sizeof(int));
 
     newgraph->variables = (Variable*) calloc(n, sizeof(Variable));
+    newgraph->var_ind = NULL;
+
     return newgraph;
 }
 
-void free_graph(Graph* G){
-    int size, i;
+Graph* copy_graph(Graph* G)
+{
+    int n;
+    Graph* G_copy;
+
+    n = G->size;
+    G_copy = new_graph(n);
+
+    memcpy(G_copy->adj_matrix, G->adj_matrix, n*n*sizeof(int));
+    memcpy(G_copy->variables, G->variables, n*sizeof(Variable));
+    if (G->var_ind == NULL)
+	   G_copy->var_ind = NULL;
+    else
+    {
+	   memcpy(G_copy->var_ind, G->var_ind, 
+              (G->min_id - G->max_id)*sizeof(int));
+	   G_copy->max_id = G->max_id;
+	   G_copy->min_id = G->min_id;
+    }
+
+    return G_copy;
+}
+
+void free_graph(Graph* G) /* XX Onko täysin valmis? */
+{
     if(G == NULL)
         return;
-    size = G->size;
-
+        
     free(G->adj_matrix);
-
-    /* Dangerous if we have add_all_variables(...) as it is now. */
     free(G->variables);
-
     free(G);
 }
 
-int add_variable(Graph* G, Variable v)
-{
-    if (G->top == G->size)
-	   return -1; /* Cannot add more items. */
-
-    G->variables[G->top] = v;
-    G->top++;
-
-    return 0; /* Error codes need work */
-}
-
-int add_all_variables(Graph* G, Variable vars[])
-{
-    free(G->variables);
-
-    /* MVK NOTE: Too dangerous ( see free_graph(...) ) */
-    G->variables = vars; /* Mostly (dangerous) syntactic syrup. */
-    /* XX Ei paluuarvoa... */
-
-    return 0;
-}
-
-int add_child(Graph* G, Variable parent, Variable child)
-{
-    int parent_i = -1, child_i = -1, i;
-
-    for (i = 0; i < G->size; i++)
-    {
-        if (G->variables[i] == parent)
-	       parent_i = i;
-      if (G->variables[i] == child)
-	       child_i = i;
-    }
-
-    if (parent_i == -1 || child_i == -1)
-	return -1;
-
-    G->adj_matrix[parent_i][child_i] = 1;
-    /* Eikä paluuarvoa */
-
-    return 0;
-}
-
+/*** GETTERS ***/
 
 int get_size(Graph* G)
 {
     return G->size;
+}
+
+Variable* get_variables(Graph* G)
+{
+    return G->variables;
+}
+
+int get_graph_index(Graph* G, Variable v)
+{
+    int i;
+
+    /* NOTE: Relies on variable ids being nearly consecutive
+     * If the implementation changes, this becomes a lot less
+     * memory efficient and needs to be modified. */
+
+    /* Voi räkä. adjmatrix ei muutu. Jospa lapsia vois lisätä vasta kun
+       kaikki muuttujat on lisätty?*/
+
+    if (G->var_ind != NULL)
+    {
+        i = G->var_ind[get_id(v) - G->min_id];
+	   return equal_variables(G->variables[i], v)? i: -1;
+    }
+    else /* Backup linear search */
+        for (i = 0; i < G->size; i++)
+	       if (equal_variables(G->variables[i], v))
+		      return i;
+
+    return -1;
 }
 
 int get_neighbours(Graph* G, Variable** neighbours, Variable V)
@@ -110,11 +110,6 @@ int get_neighbours(Graph* G, Variable** neighbours, Variable V)
     return j; /* # of neighbours */
 }
 
-Variable* get_variables(Graph* G)
-{
-    return G->variables;
-}
-
 int is_child(Graph* G, Variable parent, Variable child)
 {
     int i,j;
@@ -123,41 +118,85 @@ int is_child(Graph* G, Variable parent, Variable child)
     return ADJM(G, i, j);
 }
 
-void add_undirected_edge(Variable v1, Variable v2)
+/*** SETTERS ***/
+
+int add_variable(Graph* G, Variable v)
 {
+    if (G->top == G->size)
+	   return -1; /* Cannot add more items. */
+
+    G->variables[G->top] = v;
+    G->top++;
+
+    if (G->top == G->size)
+	sort_variables(G);
+
+    return 0; /* Error codes need work */
+}
+
+int add_child(Graph* G, Variable parent, Variable child)
+{
+    int parent_i, child_i;
+
+    parent_i = get_graph_index(G, parent);
+    child_i = get_graph_index(G, child);
+
+    if (parent_i == -1 || child_i == -1)
+	   return -1;
+
+    ADJM(G, parent_i, child_i) = 1;
+
+    return 0;
+}
+
+/*** OPERATIONS (methods) ***/
+
+int varcomp(Variable v1, Variable v2) {
+    return get_id(v1) - get_id(v2);
+}
+
+void sort_variables(Graph* G) 
+{
+    int max_id, i;
+    qsort(G->variables, G->size, sizeof(Variable), varcomp);
+    max_id = get_id(G->variables[G->size -1]);
+    G->min_id = get_id(G->variables[0]);
+    G->var_ind = (unsigned long*) calloc(max_id - G->min_id, sizeof(long));
+
+    for (i = 0; G->size; i++)
+	   G->var_ind[get_id(G->variables[i]) - G->min_id] = i;
 }
 
 Graph* make_undirected(Graph* G)
 {
     Graph* Gu;   
     int i,j,n;
-    int** Gam; /* Gam ie. G adjacency matrix */
     
     if (G == NULL || G->variables == NULL)
         return NULL;
 
     n = G->size;
-    Gam = G->adj_matrix;
-    Gu = new_graph(n);
-    add_all_variables(Gu, G->variables); /* XX Ongelmia tuhottaessa? */
-    
+    Gu = copy_graph(G);
+
     /* Create the undirected graph */
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
-            ADJM(Gu, i, j) = Gam[i*n+j] || Gam[j*n+i];
+            ADJM(Gu, i, j) = ADJM(G, i, j) || ADJM(G, j, i);
 
     return Gu;
 }
 
-void moralise(Graph* Gm, Graph* G)
+Graph* moralise(Graph* G)
 {
     int i,j,n,v;
-        
+    Graph* Gm;
+
     if (G == NULL || G->variables == NULL)
-        return;
+        return NULL;
     
     n = G->size;
-        
+    Gm = copy_graph(G);
+
     /* Moralisation */
     for (v = 0; v < n; v++)       /* Iterate variables */
         for (i = 0; i < n; i++) 
@@ -167,140 +206,71 @@ void moralise(Graph* Gm, Graph* G)
                     ADJM(Gm, i, j) |= ADJM(G, j, v);
                     ADJM(Gm, j, i) |= ADJM(G, j, v);
                 }
+                
+    return Gm;
 }
 
 /* Not specified Graph.h -- internal helper function */
 /* MVK: Pitääkö tämän olla void? Nyt siinä on ei-void palautusarvo. */
-void triangulate(Graph* Gm)
+/* AR: Joo, refaktorointia kaipaillaan */
+int triangulate(Graph* Gm, Clique** clique_p)
 {
-    /*    Graph* Gm_copy; */
-
     int i, j, j_index, k, k_index, n;
     int clique_count = 0;
+    int cluster_size;
     Variable* min_cluster;
     Heap* H;
-    Cluster_list *cl_head, *cl_end;
-    int* variable_set;
+    Cluster_list *cl_head = NULL;
+    int* variable_set; /* [i] true, if variable[i] is in the cluster */
 
     n = Gm->size;
- 
-    /* Step 1: Copy Gm */
-/*Copy of Gm probably unnecessary!
-    Gm_copy = new_graph(n);
-    for (i = 0; i < n; i++)
-	add_variable(Gm_copy, Gm->variables[i]);
-        
-    memcpy(Gm_copy->adj_matrix[0], Gm->adj_matrix[0], n*n*sizeof(int));
-*/    
-    /* Step 2: Triangulate Gm */
- 
     H = build_heap(Gm);
-
     variable_set = (int*) calloc(n, sizeof(int));
 
-    cl_head = new_cl_item(n);
-    cl_end = cl_head;
-
     for (i = 0; i < n; i++)
     {
-	min_cluster = extract_min(H, Gm);
-	
-	/* Clear the variable_set for this cluster */
-	memset(variable_set, 0, n*sizeof(int));
-	
-	for (j = 0; j < cluster_size; j++)
-	{
-	    j_index = get_graph_index(Gm, min_cluster[j]);
-	    variable_set[j_index] = 1;
-
-	    /* Add new edges to Gm. */
-	    for (k = j+1; k < cluster_size; k++)
-	    {
-            	k_index = get_graph_index(Gm, min_cluster[k]);
-
-            	ADJM(Gm, j_index, k_index) = 1;
-            	ADJM(Gm, k_index, j_index) = 1;
-	    }
-	}
-	
-	/* MVK: is_subset ei määritelty */
-	if (!is_subset(cl_head, variable_set, n))
-	{
-	    cl_end->next = new_cl_item(n);
-	    cl_end = cl_end->next;
-	    clique_count++;
-    	}
-    }
-    return cl2cliques(cl_head, clique_count, n);
-}
-
-/* MVK: Tästäkin funktiosta tarvitaan esittely. */
-Clique* cl2cliques(Cluster_list* cl_head, int n_cliques, int n)
-{
-    int n_vars, i;
-    Cluster_list* cl_i;
-    Clique* cliques = calloc(n_cliques, sizeof(Clique));
-    Variable* clique_vars = calloc(n, sizeof(Variable));
-
-    /* MVK: Mikä on vars? Ei määritelty. */
-    for (cl_i = cl_head; cl_i->next != NULL; cl_i = cl_i->next)
-    {
-	n_vars = 0;
-	for (i = 0; i < n; i++)
-	    if (cl_i->variable_set[i])
-		clique_vars[n_vars++] = vars[i];
-
-	cliques[--n_cliques] = make_Clique(clique_vars, n_vars);
-	/* This ^^^^^^^^^^^ is sort of dangerous. */
-    }
+        cluster_size = extract_min(H, Gm, &min_cluster);
+        
+        /* Clear the variable_set for this cluster */
+        memset(variable_set, 0, n*sizeof(int));
+        
+        for (j = 0; j < cluster_size; j++)
+        {
+            j_index = get_graph_index(Gm, min_cluster[j]);
+            variable_set[j_index] = 1;
     
-    return cliques;
-}
-
-/* MVK: Tästä täytyy saada esittely joko Graph.h:hon, tai tämän tiedoston
- * alkuun, koska tätä käytetään jo aiemmin. */
-Cluster_list* new_cl_item(int array_size)
-{
-    Cluster_list* cl_new = (Cluster_list*) malloc(sizeof(Cluster_list));
-    cl_new->variable_set = (int*) calloc(array_size, sizeof(int));
-    cl_new->next = NULL;
-
-    /* MVK: Palautetaan jotain. Onko oikein? */
-    return cl_new;
-}
-
-int is_subset(Cluster_list* cl_head, int* var_set, int size)
-{
-    Cluster_list* cl_i;
-    int i, flag;
-
-    /* Iterate the list of known clusters */
-    /* Later additions cannot be supersets of earlier ones */
-    /* One of the variables in an earlier var_set is removed */
-    for (cl_i = cl_head; cl_i->next != NULL; cl_i = cl_i->next)
-    {
-		flag = 0;
-		for (i = 0; i < size; i++)
-		{
-	    	if (var_set[i] & !cl_i->variable_set[i])
-	    	{
-				flag = 1; /* var_set not a subset of cl_i */
-				break;
-	    	}
-		}
-		if (!flag) /* We have a subset */
-	    	return 1;
+            /* Add new edges to Gm. */
+            for (k = j+1; k < cluster_size; k++)
+            {
+                    k_index = get_graph_index(Gm, min_cluster[k]);
+    
+                    ADJM(Gm, j_index, k_index) = 1;
+                    ADJM(Gm, k_index, j_index) = 1;
+            }
+        }
+        
+        if (!is_subset(cl_head, variable_set, n))
+        {
+            cl_head = new_cl_item(n, cl_head, variable_set);
+            clique_count++;
+        }
     }
-
-    return 0;
+    free(variable_set);
+ 
+    *clique_p = cl2cliques(Gm->variables, cl_head, clique_count, n);
+    
+    return clique_count;
 }
 
-int find_cliques(Graph* Gm, Clique* cliques_p)
+
+int find_cliques(Graph* G, Clique** cliques_p)
 {
-    /* XX NULL-tarkastukset uupuu */
+    Graph *Gu, *Gm;
+    int n_cliques;
 
-    triangulate(Gm);
+    Gu = make_undirected(G);
+    Gm = moralise(Gu);
+    n_cliques = triangulate(Gm, cliques_p);
 
-
-    return 0;
+    return n_cliques;
 }
