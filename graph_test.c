a046ed6a0bec73e475ff0a660c67dc1def4ec3e4
arasinen <arasinen>
2004-06-03 09:02:00 +0000

    Test undirected and moralisation

--- graph_test.c
+++ graph_test.c
@@ -9,7 +9,7 @@
 Graph* test1(void)
 {
     Graph *G;
-    Variable v[5];
+    Variable v[4];
     Variable *w;
     char *symbol, *name;
     
@@ -18,18 +18,22 @@ Graph* test1(void)
     printf("\tTest 1... new_graph and add_variable\n");
 
     G = new_graph(5);
+    printf("\t\tNew graph created.\n");
     
     for (i = 0; i < 5; i++)
     {
         sprintf(symbol, "var%d", i);
         sprintf(name, "Variable %d", i);
         v[i] = new_variable(symbol, name, NULL, 2);
-    
         assert(add_variable(G, v[i])==0);
     }
 
     w = get_variables(G);
+	printf("\t\tget_variables OK.\n");
+
     assert(get_size(G) == 5);
+    printf("\t\tget_size OK.\n");
+
     for (i = 0; i<5; i++) {
         assert(equal_variables(v[i], w[i]));
     }
@@ -44,21 +48,21 @@ void test2(Graph* G)
     
     printf("\tTest 2... add_child, is_child\n");
 
-    /* v1 -> v3 -> v5; v2 -> v4 -> v5 */
+    /* v0 -> v2 -> v4; v1 -> v3 -> v4 */
     
-    assert(add_child(G, v[1], v[3]) == 0);
-    assert(add_child(G, v[3], v[5]) == 0);
+    assert(add_child(G, v[0], v[2]) == 0);
     assert(add_child(G, v[2], v[4]) == 0);
-    assert(add_child(G, v[4], v[5]) == 0);
+    assert(add_child(G, v[1], v[3]) == 0);
+    assert(add_child(G, v[3], v[4]) == 0);
 
-    assert(is_child(G, v[1], v[3]));
+    assert(is_child(G, v[0], v[2]));
+    assert(is_child(G, v[1], v[3]));        
     assert(is_child(G, v[2], v[4]));        
-    assert(is_child(G, v[3], v[5]));        
-    assert(is_child(G, v[4], v[5]));        
+    assert(is_child(G, v[3], v[4]));        
     
-    assert(!is_child(G, v[1], v[1]));
-    assert(!is_child(G, v[5], v[3]));
+    assert(!is_child(G, v[0], v[0]));
     assert(!is_child(G, v[4], v[2]));
+    assert(!is_child(G, v[3], v[1]));
 
     printf("\tTest 2 done.\n");    
 }
@@ -70,7 +74,7 @@ void test3(Graph* G)
     Variable *v, *w;
     int i,j;
     
-    printf("\tTest3... copy_graph\n");
+    printf("\tTest 3... copy_graph\n");
     Gc = copy_graph(G);
     v = get_variables(G);
     w = get_variables(Gc);
@@ -82,7 +86,55 @@ void test3(Graph* G)
         for (j = 0; j < get_size(G); j++) {
             assert(is_child(G, v[i], v[j]) == is_child(G,w[i], w[j]));
         }
+    printf("\tTest 3 done.\n");
+}
+
+void test4(Graph* G)
+{
+    Graph *Gu;
+    Variable *v;
+    int i,j,n;
     
+    printf("\tTest 4... make_undirected\n");
+    Gu = make_undirected(G);
+    
+    n = get_size(G); v = get_variables(g);
+    for (i = 0; i < n; i++)
+    {
+        for (j=0;j < n;j++)
+        {
+            if (is_child(G, v[i], v[j]))
+            {
+                assert(is_child(Gu, v[i], v[j]));
+                assert(is_child(Gu, v[j], v[j]));
+            }
+            if (is_child(Gu, v[i], v[j]))
+            {
+                assert(is_child(G,v[i],v[j]) || is_child(G,v[j],v[i]));
+            }
+        }
+    }
+    printf("\tTest 4 done.\n");
+}
+
+void test5(Graph* G)
+{
+    Graph *Gm;
+    Variable *v;
+    int i,j,n;
+    printf("\tTest 5... moralise\n");
+    printf("---Not implemented yet.---\n");
+    Gm = moralise(G);
+    v = get_variables(G);
+    n = get_size(G);
+    for (i=0; i<n;i++)
+        for (j=0;j<n;j++)
+			if (is_child(G,v[i],v[j]))
+			{
+				assert(is_child(Gm, v[i], v[j]));
+			}    
+	assert(is_child(Gm, v[2], v[3]) && is_child(Gm, v[3],v[2]));
+	printf("\tTest 5 done.\n");
 }
 
 void main(void)
@@ -94,6 +146,6 @@ void main(void)
     G = test1();
     test2(G);
     test3(G);
-    
+    test4(G);
 }
 
