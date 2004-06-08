8b56eabcb8600282485c2e7455f75c3b09da4593
jatoivol <jatoivol>
2004-06-08 08:26:50 +0000

    Kirjoittelin kommentteja ja kokeilin bisontestin vaihtoehtoja...

--- bisontest.c
+++ bisontest.c
@@ -41,34 +41,35 @@ int main(int argc, char *argv[]){
   observed[1] = get_variable("D");
 
   clique_of_interest = find_family(nip_cliques, nip_num_of_cliques, 
-				   observed, 2);
+				   observed, 1);
   enter_evidence(clique_of_interest, observed[0], probB);
   printf("Entered evidence into the clique of ");
   for(i = 0; i < clique_of_interest->p->num_of_vars - 1; i++)
     printf("%s ", clique_of_interest->variables[i]->symbol);
   printf("and %s.\n", clique_of_interest->variables[i]->symbol);
 
-  /*
+
   clique_of_interest = find_family(nip_cliques, nip_num_of_cliques, 
-                                   observed, 2);
-  */
+                                   observed+1, 1);
   enter_evidence(clique_of_interest, observed[1], probD);
   printf("Entered evidence into the clique of ");
   for(i = 0; i < clique_of_interest->p->num_of_vars - 1; i++)
     printf("%s ", clique_of_interest->variables[i]->symbol);
   printf("and %s.\n", clique_of_interest->variables[i]->symbol);
 
+
   /* another propagation */
   for(i = 0; i < nip_num_of_cliques; i++)
     unmark_Clique(nip_cliques[i]);
-  collect_evidence(NULL, NULL, nip_cliques[0]);
-  distribute_evidence(nip_cliques[0]);
+  collect_evidence(NULL, NULL, clique_of_interest);
+  distribute_evidence(clique_of_interest);
+
 
   /* marginalisation */
   interesting = get_variable("A"); /* THE variable */
 
   if(!interesting){
-    printf("In bisontest.c : Interesting variable not found.\n");
+    printf("In bisontest.c : Variable of interest not found.\n");
     return 1;
   }
 
@@ -88,6 +89,7 @@ int main(int argc, char *argv[]){
 
   printf("Marginalisation returned %d. (0 is OK)\n", 
 	 marginalise(clique_of_interest, interesting, result));
+
 
   /* normalization */
   normalise(result, interesting->cardinality);
