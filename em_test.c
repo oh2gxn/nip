0b24f616961f857f9a1f2c62310c4b777a06f921
arasinen <arasinen>
2004-06-30 11:37:00 +0000

    Oppimistesti, raaka++ versio

---
+++ em_test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Graph.h"
+#include "Variable.h"
+
+void main() {
+
+    Graph *G;
+    Variable v1, v2, v3;
+
+    /* XX fundeeraa detaileja */
+
+}
