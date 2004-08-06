6857d9385efd7750ab240123f39b2644ed7ff300
mvkorpel <mvkorpel>
2004-08-06 06:36:58 +0000

    nip-juttu...

---
+++ nip.h
@@ -0,0 +1,16 @@
+/*
+ * nip.h $Id: nip.h,v 1.1 2004-08-06 06:36:58 mvkorpel Exp $
+ */
+
+#ifndef __NIP_H__
+#define __NIP_H__
+
+#include "Clique.h"
+#include "Variable.h"
+
+struct nip {
+  Clique *cliques;
+
+};
+
+#endif /* __NIP_H__ */
