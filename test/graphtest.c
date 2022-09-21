c8fe754f17cd3b24fddbac99f4da115f2b275b4e
Janne Toivola <jammu81@iki.fi>
2022-09-21 11:25:27 +0300

    test: continued systematic unit tests for graphs, moved test data to separate files

--- test/graphtest.c
+++ test/graphtest.c
@@ -30,19 +30,19 @@
 
 /* TODO: you could print an input file for graphviz (the dot program) */
 void print_adjm(nip_graph g) {
-	nip_variable* v;
-	int i,j,n;
+  nip_variable* v;
+  int i,j,n;
 
-	v = nip_graph_nodes(g);
-	n = nip_graph_size(g);
+  v = nip_graph_nodes(g);
+  n = nip_graph_size(g);
 
-	/*printf("\t\tAdjacency matrix:\n");*/
-	for (i = 0; i < n; i++) {
-	  printf("\t\t");
-	  for (j=0; j < n; j++)
-	    printf("%d ", nip_graph_linked(g, v[i], v[j]));
-	  printf("\n");
-	}
+  /*printf("\t\tAdjacency matrix:\n");*/
+  for (i = 0; i < n; i++) {
+    printf("\t\t");
+    for (j=0; j < n; j++)
+      printf("%d ", nip_graph_linked(g, v[i], v[j]));
+    printf("\n");
+  }
 }
 
 nip_graph test1() {
@@ -55,14 +55,14 @@ nip_graph test1() {
   char* states[] = {"0", "1"};
   /* MVK: Must reserve some memory (?)*/
   /* AR: True. IRIX has a black magic sprintf.*/
-  
+
   int i;
   
   printf("\tTest 1... new_graph and add_node\n");
-  
+
   g = nip_new_graph(5);
   printf("\t\tNew graph created.\n");
-  
+
   for (i = 0; i < 5; i++) {
     sprintf(symbol, "var%d", i);
     sprintf(name, "variable %d", i);
@@ -72,201 +72,200 @@ nip_graph test1() {
 
   w = nip_graph_nodes(g);
   printf("\t\tgraph_nodes OK.\n");
-  
+
   assert(nip_graph_size(g) == 5);
   printf("\t\tgraph_size OK.\n");
 
   for (i = 0; i<5; i++) {
     assert(nip_equal_variables(v[i], w[i]));
   }
-  
+
   printf("\tTest 1 done.\n");
   return g;
 }
 
 void test2(nip_graph g) {
-    nip_variable* v = nip_graph_nodes(g);
-    
-    printf("\tTest 2... add_child(), linked()\n");
+  nip_variable* v = nip_graph_nodes(g);
+  
+  printf("\tTest 2... add_child(), linked()\n");
 
-    /* v0 -> v2 -> v4; v1 -> v3 -> v4 */
-    
-    assert(nip_graph_add_child(g, v[0], v[2]) == 0);
-    assert(nip_graph_add_child(g, v[2], v[4]) == 0);
-    assert(nip_graph_add_child(g, v[1], v[3]) == 0);
-    assert(nip_graph_add_child(g, v[3], v[4]) == 0);
+  /* v0 -> v2 -> v4; v1 -> v3 -> v4 */
+  
+  assert(nip_graph_add_child(g, v[0], v[2]) == 0);
+  assert(nip_graph_add_child(g, v[2], v[4]) == 0);
+  assert(nip_graph_add_child(g, v[1], v[3]) == 0);
+  assert(nip_graph_add_child(g, v[3], v[4]) == 0);
 
-    assert(nip_graph_linked(g, v[0], v[2]));
-    assert(nip_graph_linked(g, v[1], v[3]));        
-    assert(nip_graph_linked(g, v[2], v[4]));        
-    assert(nip_graph_linked(g, v[3], v[4]));        
-    
-    assert(!nip_graph_linked(g, v[0], v[0]));
-    assert(!nip_graph_linked(g, v[4], v[2]));
-    assert(!nip_graph_linked(g, v[3], v[1]));
+  assert(nip_graph_linked(g, v[0], v[2]));
+  assert(nip_graph_linked(g, v[1], v[3]));        
+  assert(nip_graph_linked(g, v[2], v[4]));        
+  assert(nip_graph_linked(g, v[3], v[4]));        
+  
+  assert(!nip_graph_linked(g, v[0], v[0]));
+  assert(!nip_graph_linked(g, v[4], v[2]));
+  assert(!nip_graph_linked(g, v[3], v[1]));
 
-    printf("\tTest 2 done.\n");    
+  printf("\tTest 2 done.\n");    
 }
 
 
 void test3(nip_graph g) {
-    nip_graph gc;
-    nip_variable *v, *w;
-    int i,j,n;
-    
-    printf("\tTest 3... copy_graph\n");
-    gc = nip_copy_graph(g);
-    v = nip_graph_nodes(g);
-    w = nip_graph_nodes(gc);
-    n = nip_graph_size(g);
-    assert(n == nip_graph_size(gc));
-    for (i = 0; i < n; i++) {
-        assert(nip_equal_variables(v[i],w[i]));
+  nip_graph gc;
+  nip_variable *v, *w;
+  int i,j,n;
+  
+  printf("\tTest 3... copy_graph\n");
+  gc = nip_copy_graph(g);
+  v = nip_graph_nodes(g);
+  w = nip_graph_nodes(gc);
+  n = nip_graph_size(g);
+  assert(n == nip_graph_size(gc));
+  for (i = 0; i < n; i++) {
+    assert(nip_equal_variables(v[i],w[i]));
+  }
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++) {
+      assert(nip_graph_linked(g, v[i], v[j]) == 
+             nip_graph_linked(g, w[i], w[j]));
     }
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++) {
-	  assert(nip_graph_linked(g, v[i], v[j]) == 
-		 nip_graph_linked(g, w[i], w[j]));
-        }
-    nip_free_graph(gc);
-    printf("\tTest 3 done.\n");
+  nip_free_graph(gc);
+  printf("\tTest 3 done.\n");
 }
 
 void test4(nip_graph g) {
-    nip_graph gu;
-    nip_variable* v;
-    int i,j,n;
-    
-    printf("\tTest 4... make_graph_undirected\n");
-    gu = nip_make_graph_undirected(g);
-    
-    n = nip_graph_size(g);
-    v = nip_graph_nodes(g);
-    for (i = 0; i < n; i++) {
-      for (j=0;j < n;j++) {
-	if (nip_graph_linked(g, v[i], v[j])) {
-	  /* printf("%s -> %s\n", v[i]->symbol, v[j]->symbol); */
-	  assert(nip_graph_linked(gu, v[i], v[j]));
-	  assert(nip_graph_linked(gu, v[j], v[i]));
-	}
-	if (nip_graph_linked(gu, v[i], v[j])) {
-	  assert(nip_graph_linked(g,v[i],v[j]) || 
-		 nip_graph_linked(g,v[j],v[i]));
-	}
+  nip_graph gu;
+  nip_variable* v;
+  int i,j,n;
+  
+  printf("\tTest 4... make_graph_undirected\n");
+  gu = nip_make_graph_undirected(g);
+  
+  n = nip_graph_size(g);
+  v = nip_graph_nodes(g);
+  for (i = 0; i < n; i++) {
+    for (j=0;j < n;j++) {
+      if (nip_graph_linked(g, v[i], v[j])) {
+        /* printf("%s -> %s\n", v[i]->symbol, v[j]->symbol); */
+        assert(nip_graph_linked(gu, v[i], v[j]));
+        assert(nip_graph_linked(gu, v[j], v[i]));
+      }
+      if (nip_graph_linked(gu, v[i], v[j])) {
+        assert(nip_graph_linked(g,v[i],v[j]) || 
+               nip_graph_linked(g,v[j],v[i]));
       }
     }
-    nip_free_graph(gu);
-    printf("\tTest 4 done.\n");
+  }
+  nip_free_graph(gu);
+  printf("\tTest 4 done.\n");
 }
 
 void test5(nip_graph g) {
-    nip_graph gm;
-    nip_variable* v;
-    int i,j,n;
-    printf("\tTest 5... moralise\n");
-    gm = nip_moralise_graph(g);
-    v = nip_graph_nodes(g);
-    n = nip_graph_size(g);
-    for (i=0; i<n; i++)
-        for (j=0; j<n; j++)
-	    if (nip_graph_linked(g,v[i],v[j])) {
-	        assert(nip_graph_linked(gm, v[i], v[j]));
-	    }    
-    assert(nip_graph_linked(gm, v[2], v[3]) && 
-	   nip_graph_linked(gm, v[3], v[2]));
+  nip_graph gm;
+  nip_variable* v;
+  int i,j,n;
+  printf("\tTest 5... moralise\n");
+  gm = nip_moralise_graph(g);
+  v = nip_graph_nodes(g);
+  n = nip_graph_size(g);
+  for (i=0; i<n; i++)
+    for (j=0; j<n; j++)
+      if (nip_graph_linked(g,v[i],v[j])) {
+        assert(nip_graph_linked(gm, v[i], v[j]));
+      }    
+  assert(nip_graph_linked(gm, v[2], v[3]) && 
+         nip_graph_linked(gm, v[3], v[2]));
 
-    nip_free_graph(gm);
-    printf("\tTest 5 done.\n");
+  nip_free_graph(gm);
+  printf("\tTest 5 done.\n");
 }
 
 nip_graph test6() {
-	nip_graph g, gu, gm;
-	int i,n = 8;
-	char* states[] = {"0", "1"};
-	nip_clique* cliques;
-	nip_variable v[8];
+  nip_graph g, gu, gm;
+  int i,n = 8;
+  char* states[] = {"0", "1"};
+  nip_clique* cliques;
+  nip_variable v[8];
 	
-	printf("\tTest 6... triangulation\n");
-	g = nip_new_graph(n);
+  printf("\tTest 6... triangulation\n");
+  g = nip_new_graph(n);
 	
-	v[0] =  nip_new_variable("A", "", states, 2);
-	v[1] =  nip_new_variable("B", "", states, 2);
-	v[2] =  nip_new_variable("C", "", states, 2);
-	v[3] =  nip_new_variable("D", "", states, 2);
-	v[4] =  nip_new_variable("E", "", states, 2);
-	v[5] =  nip_new_variable("F", "", states, 2);
-	v[6] =  nip_new_variable("G", "", states, 2);
-	v[7] =  nip_new_variable("H", "", states, 2);
-	/* these variables get freed before g is freed, I hope */
+  v[0] =  nip_new_variable("A", "", states, 2);
+  v[1] =  nip_new_variable("B", "", states, 2);
+  v[2] =  nip_new_variable("C", "", states, 2);
+  v[3] =  nip_new_variable("D", "", states, 2);
+  v[4] =  nip_new_variable("E", "", states, 2);
+  v[5] =  nip_new_variable("F", "", states, 2);
+  v[6] =  nip_new_variable("G", "", states, 2);
+  v[7] =  nip_new_variable("H", "", states, 2);
+  /* these variables get freed before g is freed, I hope */
 
-	for (i=0; i<n; i++)
-	    nip_graph_add_node(g, v[i]);
+  for (i=0; i<n; i++)
+    nip_graph_add_node(g, v[i]);
 	
-	nip_graph_add_child(g, v[0],v[1]);
-	nip_graph_add_child(g, v[0],v[2]);
-	nip_graph_add_child(g, v[1],v[3]);	
-	nip_graph_add_child(g, v[2],v[4]);
-	nip_graph_add_child(g, v[2],v[6]);	
-	nip_graph_add_child(g, v[3],v[5]);
-	nip_graph_add_child(g, v[4],v[5]);
-	nip_graph_add_child(g, v[4],v[7]);
-	nip_graph_add_child(g, v[6],v[7]);	
+  nip_graph_add_child(g, v[0],v[1]);
+  nip_graph_add_child(g, v[0],v[2]);
+  nip_graph_add_child(g, v[1],v[3]);	
+  nip_graph_add_child(g, v[2],v[4]);
+  nip_graph_add_child(g, v[2],v[6]);	
+  nip_graph_add_child(g, v[3],v[5]);
+  nip_graph_add_child(g, v[4],v[5]);
+  nip_graph_add_child(g, v[4],v[7]);
+  nip_graph_add_child(g, v[6],v[7]);	
 
-	printf("\t\tOriginal graph:\n");
-	print_adjm(g);
-	gm = nip_moralise_graph(g);
-	gu = nip_make_graph_undirected(gm);
-	nip_free_graph(gm);
-	printf("\t\tMoral and undirected graph:\n");
-	print_adjm(gu);
+  printf("\t\tOriginal graph:\n");
+  print_adjm(g);
+  gm = nip_moralise_graph(g);
+  gu = nip_make_graph_undirected(gm);
+  nip_free_graph(gm);
+  printf("\t\tMoral and undirected graph:\n");
+  print_adjm(gu);
 
-	/* construct cliques */
-	n = nip_triangulate_graph(gu, &cliques);
-	printf("\t\tTriangulated graph:\n");
-	print_adjm(gu);
-	/* free cliques */
-	for(i = 0; i<n; i++)
-	  nip_free_clique(cliques[i]);
-	free(cliques);
+  /* construct cliques */
+  n = nip_triangulate_graph(gu, &cliques);
+  printf("\t\tTriangulated graph:\n");
+  print_adjm(gu);
+  /* free cliques */
+  for(i = 0; i<n; i++)
+    nip_free_clique(cliques[i]);
+  free(cliques);
 	
-	printf("\tTest 6 done.\n");
-	nip_free_graph(gu);
-	return g;
+  printf("\tTest 6 done.\n");
+  nip_free_graph(gu);
+  return g;
 }
 
 void test7(nip_graph g) {
-	nip_clique* cliques;
-	nip_clique ci;
-	int i, j, n_cliques, n_vars;
+  nip_clique* cliques;
+  nip_clique ci;
+  int i, j, n_cliques, n_vars;
 	
-	printf("\tTest 7... graph_to_cliques\n");
+  printf("\tTest 7... graph_to_cliques\n");
 
-	/* construct cliques */
-	n_cliques = nip_graph_to_cliques(g, &cliques);
-	for (i = 0; i < n_cliques; i++) {
-		printf("\t\tclique %i: ", i);
-		ci = cliques[i];
-		n_vars = nip_clique_size(ci);
-		for (j = 0; j < n_vars; j++)
-			printf("%s ", ci->variables[j]->symbol);
-		printf("\n");	
-	}
+  /* construct cliques */
+  n_cliques = nip_graph_to_cliques(g, &cliques);
+  for (i = 0; i < n_cliques; i++) {
+    printf("\t\tclique %i: ", i);
+    ci = cliques[i];
+    n_vars = nip_clique_size(ci);
+    for (j = 0; j < n_vars; j++)
+      printf("%s ", ci->variables[j]->symbol);
+    printf("\n");	
+  }
 	
-	/* free the cliques */
-	for (i = 0; i < n_cliques; i++)
-	  nip_free_clique(cliques[i]);
-	free(cliques);
+  /* free the cliques */
+  for (i = 0; i < n_cliques; i++)
+    nip_free_clique(cliques[i]);
+  free(cliques);
 
-	printf("\tTest 7 done.\n");	
+  printf("\tTest 7 done.\n");	
 }
 
 int main(void) {
   int i,n;
   nip_variable* v;
   nip_graph g, g2;
-  printf("-------------------------------\n");
   printf("Testing graphs:\n");
-  
+
   g = test1();
   test2(g);
   test3(g);
@@ -286,7 +285,6 @@ int main(void) {
   for (i=0; i<n; i++)
     nip_free_variable(v[i]);
   nip_free_graph(g2);
-  
+
   return 0;
 }
-
--- tests.sh
+++ tests.sh
@@ -37,91 +37,39 @@ echo 'NIP tests' 1>&2
 echo '0. Compile the source code' 1>&2
 make
 
+
 echo '' 1>&2
 echo '1. Test low-level string tokenization:' 1>&2
 
+if=test/input1.txt
 of=test/output1.txt
 ef=test/expect1.txt
-echo "foo (bar) { };" | ./test/stringtest > $of
-printf "foo (bar) { };\n\n0 3 4 9 10 11 12 14 \n1 lines, 4 words, 16 chars\n4 first words:\nfoo\n(bar)\n{\n};\n" > $ef
+head -n1 $if | ./test/stringtest > $of
+tail -n+2 $if | ./test/stringtest >> $of
 assert $of $ef $LINENO
-rm $of $ef
+rm $of
 
-printf "potential (A | B) {\n  data = (( 1.0 0.0 )\n  (0.1 0.9));\n}\n" | ./test/stringtest > $of
-printf "potential (A | B) {\n\n0 9 10 12 13 14 15 17 18 19 \n  data = (( 1.0 0.0 )\n\n2 6 7 8 9 11 12 15 16 19 20 21 \n  (0.1 0.9));\n\n2 6 7 13 \n}\n\n0 1 \n4 lines, 14 words, 62 chars\n14 first words:\npotential\n(A\n|\nB)\n{\ndata\n=\n((\n1.0\n0.0\n)\n(0.1\n0.9));\n}\n" > $ef
-assert $of $ef $LINENO
-rm $of $ef
 
 echo '' 1>&2
 echo '2. Test string tokenization for parsing models:' 1>&2
 
-if=test/model.net
+if=test/input2.net
 of=test/output2.txt
 ef=test/expect2.txt
-printf "potential (x1 | x2) { data={\n  0.9 0.1\n  0.1 0.9\n} }\n" > $if
-echo "$if:" > $ef
-echo "potential" >> $ef
-echo "(" >> $ef
-echo "x1" >> $ef
-echo "|" >> $ef
-echo "x2" >> $ef
-echo ")" >> $ef
-echo "{" >> $ef
-echo "data" >> $ef
-echo "=" >> $ef
-echo "{" >> $ef
-echo "0.9" >> $ef
-echo "0.1" >> $ef
-echo "0.1" >> $ef
-echo "0.9" >> $ef
-echo "}" >> $ef
-echo "}" >> $ef
-
 ./test/parsertest $if > $of
 assert $of $ef $LINENO
-rm $if $of $ef
+rm $of
 
 
 echo '' 1>&2
 echo '3. Test string tokenization for parsing data:' 1>&2
 
-if=test/data.csv
+if=test/input3.txt
 of=test/output3.txt
 ef=test/expect3.txt
-printf "\n" > $if
-printf "x1,x2\n\n" >> $if
-printf "1,foo\n" >> $if
-printf "1,foo\n" >> $if
-printf "1,bar\n" >> $if
-printf "\n" >> $if
-printf "2,foo\n" >> $if
-printf "null,bar\n" >> $if
-printf "\n\n" >> $if
-printf "2,bar\n" >> $if
-
-echo "Information about the data file:" > $ef
-echo "Name: $if" >> $ef
-echo "Separator: ," >> $ef
-echo "is_open: 1" >> $ef
-echo "label_line: 2" >> $ef
-echo "Number of sequences: 3" >> $ef
-echo "Sequence lengths: 3,2,1" >> $ef
-echo "Number of columns: 2" >> $ef
-echo "Columns:" >> $ef
-echo "- Node x1, states: 2,1" >> $ef
-echo "- Node x2, states: bar,foo" >> $ef
-echo "" >> $ef
-echo "Reading tokens one line at a time:" >> $ef
-echo "1,foo" >> $ef
-echo "1,foo" >> $ef
-echo "1,bar" >> $ef
-echo "2,foo" >> $ef
-echo "null,bar" >> $ef
-echo "2,bar" >> $ef
-
 ./test/datafiletest $if > $of
 assert $of $ef $LINENO
-rm $if $of $ef
+rm $of
 
 
 echo '' 1>&2
@@ -129,47 +77,9 @@ echo '4. Test belief potentials:' 1>&2
 
 of=test/output4.txt
 ef=test/expect4.txt
-echo "Mapping:" > $ef
-parallel echo "P\({1}\, {2}\, {3}\) =" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/index.txt
-parallel echo "12*{1}+4*{2}+{3} | bc" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Inverse mapping:" >> $ef
-parallel echo "\-\-\> \[{3}\, {2}\, {1}\]" ::: 0 1 2 3 ::: 0 1 2 ::: 0 1 > test/index.txt
-paste -d' ' test/values.txt test/index.txt >> $ef
-echo "Marginalized:" >> $ef
-parallel echo "P\({1}\, {2}\) =" ::: 0 1 ::: 0 1 2 3 > test/index.txt
-parallel echo {} ::: 12 15 18 21 48 51 54 57 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Copy:" >> $ef
-parallel echo "P\({1}\, {2}\, {3}\) =" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/index.txt
-parallel echo "12*{1}+4*{2}+{3} | bc" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Sum:" >> $ef
-parallel echo "P\({1}\, {2}\, {3}\) =" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/index.txt
-parallel echo "2*\(12*{1}+4*{2}+{3}\) | bc" ::: 0 1 ::: 0 1 2 ::: 0 1 2 3 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Margin:" >> $ef
-echo "P(+, 0, +) = 120" >> $ef
-echo "P(+, 1, +) = 184" >> $ef
-echo "P(+, 2, +) = 248" >> $ef
-echo "CPD:" >> $ef
-parallel echo "P\({3}\, {2}\, {1}\) =" ::: 0 1 2 3 ::: 0 1 2 ::: 0 1 > test/index.txt
-parallel echo {} ::: 0.000000 1.000000 0.200000 0.800000 0.285714 0.714286 0.071429 0.928571 0.227273 0.772727 0.300000 0.700000 0.125000 0.875000 0.250000 0.750000 0.312500 0.687500 0.166667 0.833333 0.269231 0.730769 0.323529 0.676471 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Belief update from another potential:" >> $ef
-parallel echo {} ::: 0.000000 0.250000 0.333333 0.333333 0.666667 0.416667 0.066667 0.254902 0.333333 0.333333 0.600000 0.411765 0.111111 0.259259 0.333333 0.333333 0.555556 0.407407 0.142857 0.263158 0.333333 0.333333 0.523810 0.403509 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Belief update from single margin:" >> $ef
-parallel echo {} ::: 0.000000 0.516667 0.333333 0.333333 0.322581 0.201613 0.137778 0.526797 0.333333 0.333333 0.290323 0.199241 0.229630 0.535802 0.333333 0.333333 0.268817 0.197133 0.295238 0.543860 0.333333 0.333333 0.253456 0.195246 > test/values.txt
-paste -d' ' test/index.txt test/values.txt >> $ef
-echo "Normalize:" >> $ef
-echo "P(+, 0, +) = 1" >> $ef
-echo "P(+, 1, +) = 1" >> $ef
-echo "P(+, 2, +) = 1" >> $ef
-
 ./test/potentialtest > $of
 assert $of $ef $LINENO
-rm $of $ef test/index.txt test/values.txt
+rm $of
 
 
 echo '' 1>&2
@@ -177,16 +87,19 @@ echo '5. Test join tree inference:' 1>&2
 
 of=test/output5.txt
 ef=test/expect5.txt
-parallel echo "Correct={}, Reordered={}" ::: 0.4 0.5 0.5 0.6 0.6 0.5 0.5 0.4 0.2 0.4 0.6 0.7 0.8 0.6 0.4 0.3 0.1 0.3 0.6 0.9 0.9 0.7 0.4 0.1 > $ef
-echo "Marginalisation returned 0. (0 is OK)" >> $ef
-echo "Normalised probability of A:" >> $ef
-echo "result[0] = 0.49225" >> $ef
-echo "result[1] = 0.2973"  >> $ef
-echo "result[2] = 0.21045" >> $ef
-
 ./test/cliquetest > $of
 assert $of $ef $LINENO
-rm $of $ef
+rm $of
+
+
+echo '' 1>&2
+echo '6. Test graph operations:' 1>&2
+
+of=test/output6.txt
+ef=test/expect6.txt
+./test/graphtest > $of
+assert $of $ef $LINENO
+rm $of
 
 
 # TODO: some 9 layers or units more...
